89dd06d6c78cbb2e5eb372931152e76b9ac656cf
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-03-03 11:30:09 -0500

    Get simple menu set up

--- rmsd_v2/RMSDV2.py
+++ rmsd_v2/RMSDV2.py
@@ -1,21 +1,148 @@
 import tempfile
+from os import path
+from tkinter import Menu
 from Bio.PDB import Superimposer, PDBParser
-from Bio import SeqIO, pairwise2
+from Bio import pairwise2
 from Bio.Data.SCOPData import protein_letters_3to1 as aa3to1
 from Bio.PDB.Polypeptide import is_aa
 from Bio.Align import substitution_matrices
 
 import nanome
-from nanome.util import Logs, async_callback
+from nanome.api import ui
+from nanome.api.structure import Complex, Chain
+from nanome.util import Logs, async_callback, Vector3, ComplexUtils
+
+BASE_PATH = path.dirname(f'{path.realpath(__file__)}')
+MENU_PATH = path.join(BASE_PATH, 'menu.json')
+
+
+class RMSDMenu:
+
+    def __init__(self, plugin_instance):
+        super().__init__()
+        self._menu = ui.Menu.io.from_json(MENU_PATH)
+        self.plugin = plugin_instance
+
+    @property
+    def ln_struct1_complex(self):
+        return self._menu.root.find_node('ln_struct1_complex')
+    
+    @property
+    def ln_struct1_chain(self):
+        return self._menu.root.find_node('ln_struct1_chain')
+
+    @property
+    def ln_struct2_complex(self):
+        return self._menu.root.find_node('ln_struct2_complex')
+    
+    @property
+    def ln_struct2_chain(self):
+        return self._menu.root.find_node('ln_struct2_chain')
+
+    @async_callback
+    async def render(self, complexes=None, default_values=False):
+        complexes = complexes or []
+        self.complexes = complexes
+
+        # for comp in self.complexes:
+        #     comp.register_complex_updated_callback(self.on_complex_updated)
+
+        # Find the first complex with selected atoms, and make that the default.
+        # I guess that works for now.
+        default_complex = None
+        if complexes:
+            default_complex = complexes[0]
+
+        self.display_structures(complexes, self.ln_struct1_complex, default_structure=default_complex)
+        if default_complex:
+            await self.display_chains(default_complex, self.ln_struct1_chain)
+
+        self.display_structures(complexes, self.ln_struct2_complex, default_structure=default_complex)
+        if default_complex:
+            await self.display_chains(default_complex, self.ln_struct2_chain)
+
+        # self.display_structures(complexes, self.ln_ligands)
+        # Determine whether we should currently be showing the ligand dropdown.
+        # enable_ligands_node = self.btn_show_all_interactions.selected
+        # self.toggle_ln_ligands_visibility(enable_ligands_node)
+        # self.dd_complexes = self.ln_complexes.get_content()
+        # self.dd_ligands = self.ln_ligands.get_content()
+        # self.dd_ligands.register_item_clicked_callback(self.update_dropdown)
+        # self.dd_complexes.register_item_clicked_callback(self.toggle_complex)
+        self.plugin.update_menu(self._menu)
+
+    def display_structures(self, complexes, layoutnode, default_structure=False):
+        """Create dropdown of complexes, and add to provided layoutnode."""
+        dropdown_items = self.create_structure_dropdown_items(complexes)
+        dropdown = ui.Dropdown()
+        dropdown.max_displayed_items = 12
+        dropdown.items = dropdown_items
+
+        # set default item selected.
+        if default_structure:
+            for ddi in dropdown.items:
+                select_ddi = False
+                if isinstance(default_structure, Complex):
+                    select_ddi = ddi.complex.index == default_structure.index
+
+                if select_ddi:
+                    ddi.selected = True
+                    break
+
+        layoutnode.set_content(dropdown)
+        self.plugin.update_node(layoutnode)
+    
+    async def display_chains(self, complex, layoutnode):
+        """Create dropdown of chains, and add to provided layoutnode."""
+        dropdown = ui.Dropdown()
+        dropdown_items = []
+        if sum(1 for ch in complex.chains) == 0:
+            # get deep complex
+            complex = (await self.plugin.request_complexes([complex.index]))[0]
+        chain_names = [ch.name for ch in complex.chains]
+        for chain_name in chain_names:
+            ddi = ui.DropdownItem(chain_name)
+            dropdown_items.append(ddi)
+        dropdown.items = dropdown_items
+        layoutnode.set_content(dropdown)
+        self.plugin.update_node(layoutnode)
+
+    def create_structure_dropdown_items(self, complexes):
+        """Generate list of buttons corresponding to provided complexes."""
+        complex_ddis = []
+        ddi_labels = set()
+
+        for struct in complexes:
+            struct_name = struct.full_name
+
+            # Make sure we have a unique name for every structure
+            ddi_label = struct_name
+            if ddi_label in ddi_labels:
+                num = 1
+                while ddi_label in ddi_labels:
+                    ddi_label = f'{struct_name} {{{num}}}'
+                    num += 1
+
+            ddi_labels.add(ddi_label)
+            ddi = ui.DropdownItem(ddi_label)
+            ddi.complex = struct
+            complex_ddis.append(ddi)
+
+        return complex_ddis
 
 
 class RMSDV2(nanome.AsyncPluginInstance):
 
     def start(self):
-        pass
+        self.menu = RMSDMenu(self)
 
     @async_callback
     async def on_run(self):
+        self.menu.enabled = True
+        complexes = await self.request_complex_list()
+        Logs.message('RMSDV2 Run.')
+        self.menu.render(complexes=complexes)
+        return
         Logs.message("Retrieving complexes")
         complexes = await self.request_complex_list()
         complexes = await self.request_complexes([comp.index for comp in complexes])
@@ -24,12 +151,20 @@ class RMSDV2(nanome.AsyncPluginInstance):
         parser = PDBParser(QUIET=True)
         fixed_comp = complexes[0]
         moving_comp = complexes[1]
+        ComplexUtils.align_to(moving_comp, fixed_comp)
+
         fixed_pdb = tempfile.NamedTemporaryFile(suffix=".pdb")
         moving_pdb = tempfile.NamedTemporaryFile(suffix=".pdb")
         fixed_comp.io.to_pdb(fixed_pdb.name)
         moving_comp.io.to_pdb(moving_pdb.name)
         fixed_struct = parser.get_structure(fixed_comp.full_name, fixed_pdb.name)
         moving_struct = parser.get_structure(moving_comp.full_name, moving_pdb.name)
+        # Logs.message("Selecting Chains")
+        # fixed_chain = next(fixed_struct.get_chains())
+        # moving_chain = next(moving_struct.get_chains())
+        # fixed_chain_name = 'A'
+        # moving_chain_name = 'A'
+
         Logs.message("Aligning Structures.")
         mapping = self.align_sequences(fixed_struct, moving_struct)
 
@@ -44,15 +179,25 @@ class RMSDV2(nanome.AsyncPluginInstance):
                     if atom.serial_number == moving_atom_serial)
                 moving_atoms.append(moving_atom)
 
-        # Logs.message("Selecting Chains")
-        # fixed_chain = next(fixed_struct.get_chains())
-        # moving_chain = next(moving_struct.get_chains())
-        # fixed_chain_name = 'A'
-        # moving_chain_name = 'A'
         Logs.message("Superimposing Structures.")
         superimposer = Superimposer()
         superimposer.set_atoms(fixed_atoms, moving_atoms)
-        Logs.message(superimposer.rms)
+        superimposer.apply([*fixed_atoms, *moving_atoms])
+        Logs.message(f'RMSD: {superimposer.rms}')
+        Logs.message("Updating Workspace")
+        # Apply changes to Workspace
+        for comp_atom in moving_comp.atoms:
+            struc_atom = next((a for a in moving_atoms if comp_atom.serial == a.serial_number), None)
+            if not struc_atom:
+                continue
+            comp_atom.position = Vector3(*struc_atom.coord)
+
+        for comp_atom in fixed_comp.atoms:
+            struc_atom = next((a for a in moving_atoms if comp_atom.serial == a.serial_number), None)
+            if not struc_atom:
+                continue
+            comp_atom.position = Vector3(*struc_atom.coord)
+        await self.update_structures_deep([moving_struct])
 
     def align_sequences(self, structA, structB):
         """
