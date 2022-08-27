3b1c5121ef51b9458911bd834028cd3fba07a15d
Mike Rosengrant <mike.rosengrant@nanome.ai>
2022-08-26 20:40:25 -0400

    refactor

--- plugin/SuperimposePlugin.py
+++ plugin/SuperimposePlugin.py
@@ -30,7 +30,6 @@ class SuperimposePlugin(nanome.AsyncPluginInstance):
 
     @async_callback
     async def on_run(self):
-        self.menu.prepare()
         self.menu.open_menu()
         if not self.complexes:
             self.set_plugin_list_button(enums.PluginListButtonType.run, text='Loading...', usable=False)
